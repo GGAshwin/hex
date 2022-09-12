80b0a2e61da23b108a9d65916d0425ff7b902aec
playmaker159 <ashwinprabhu2001@gmail.com>
2022-09-12 20:35:38 +0530

    small design changes

--- hex.js
+++ hex.js
@@ -1,15 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const color = document.querySelector('#color');
+const copy=document.querySelector('#copy')
+
 btn.addEventListener('click', function () {
     let hexcolor = "#";
     for (let i = 0; i < 6; i++) {
         hexcolor += hex[getrandomNumber()];
     }
-    document.getElementById('color').innerText = hexcolor;
+    document.getElementById('color').value = hexcolor;
     document.body.style.backgroundColor = hexcolor;
 })
 
 function getrandomNumber() {
     return Math.floor(Math.random() * hex.length);
 }
+
+copy.addEventListener('click',()=>{
+    color.select();
+    navigator.clipboard.writeText(color.value)
+    alert("Color copied")
+})
